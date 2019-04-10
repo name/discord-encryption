bcb52bf41448fc2ef786dc108ab8cd12b38213db
charlie <charlie@maddex.co>
2019-04-10 21:00:28 +0100

    Created script

---
+++ encryption.js
@@ -0,0 +1,14 @@
+//META{"name":"encryption"}*//
+class encryption {
+    load() {
+
+        //  add crypto lib + some useful functions
+        $("head").append(`
+      			<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/sjcl/1.0.7/sjcl.min.js"></script>
+      			<script type="text/javascript" src="https://mwittrien.github.io/BetterDiscordAddons/Plugins/BDfunctionsDevilBro.js"></script>
+		    `);
+
+    }
+
+
+};
